56a3bfff1186c999ddc25c104b532c3913eeb95f
Mario <mario.balukcic1@gmail.com>
2021-01-12 16:27:19 +0100

    Added esbuild with jest

--- packages/craco-esbuild/src/index.js
+++ packages/craco-esbuild/src/index.js
@@ -52,12 +52,19 @@ module.exports = {
   /**
    * To process the js/ts files we replace the babel-loader with the esbuild jest loader
    */
-  overrideJestConfig: ({ jestConfig }) => {
-    // TODO add back once https://github.com/aelbore/esbuild-jest/issues/7 is fixed
-    // // Replace babel transform with esbuild
-    // const key = Object.keys(jestConfig.transform)[0];
-    // jestConfig.transform[key] = [require.resolve("esbuild-jest")];
+  overrideJestConfig: ({ jestConfig, context: { paths } }) => {
+    const useTypeScript = fs.existsSync(paths.appTsConfig);
+
+    // Replace babel transform with esbuild
+    const key = Object.keys(jestConfig.transform)[0];
+    jestConfig.transform[key] = [
+      require.resolve("esbuild-jest"),
+      {
+        loader: useTypeScript ? "tsx" : "jsx",
+      },
+    ];
 
     return jestConfig;
+
   },
 };
