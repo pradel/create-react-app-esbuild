4b032d1c481ca91302887cc324d1b86146e1d242
pradel <pradel.leo@gmail.com>
2021-01-18 22:30:37 +0100

    chore: automate release

---
+++ .versionrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  skip: {
+    // This is handled by the bash script
+    commit: true,
+    tag: true,
+  },
+  // Where we read the current version
+  packageFiles: "packages/craco-esbuild/package.json",
+  // Empty array as we update the version in the release bash script
+  bumpFiles: [],
+  // Configuration passed down to the preset
+  types: [
+    {
+      type: "feat",
+      section: "Features",
+    },
+    {
+      type: "fix",
+      section: "Bug Fixes",
+    },
+    {
+      type: "perf",
+      section: "Performance Improvements",
+    },
+    {
+      type: "revert",
+      section: "Reverts",
+    },
+    {
+      type: "docs",
+      section: "Documentation",
+    },
+    {
+      type: "chore",
+      section: "Miscellaneous Chores",
+    },
+    {
+      type: "refactor",
+      section: "Code Refactoring",
+    },
+    {
+      type: "test",
+      section: "Tests",
+    },
+    {
+      type: "build",
+      section: "Build System",
+    },
+    {
+      type: "ci",
+      section: "Continuous Integration",
+    },
+  ],
+};
