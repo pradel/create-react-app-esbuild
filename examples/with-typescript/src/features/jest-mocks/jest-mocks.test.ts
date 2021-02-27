95dbb48ee869ff539f63f5b62f5a285803e067e3
pradel <pradel.leo@gmail.com>
2021-02-27 12:05:51 +0100

    Update jest-mocks.test.ts

--- examples/with-typescript/src/features/jest-mocks/jest-mocks.test.ts
+++ examples/with-typescript/src/features/jest-mocks/jest-mocks.test.ts
@@ -1,6 +1,6 @@
 import { jestMockWorking } from './jest-mocks';
 
-jest.mock('jest-mocks');
+jest.mock('./jest-mocks');
 
 test('Value should be true if mocks is working', () => {
   expect(jestMockWorking).toBe(true);
